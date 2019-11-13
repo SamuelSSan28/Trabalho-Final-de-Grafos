8f78f9b1c7459b0e61fbd807d67bf3affc559a74
Samuel Santos <samuelssan28@gmail.com>
2019-11-12 22:04:24 -0300

    Dijkstra bunitinho

--- graph.py
+++ graph.py
@@ -2,7 +2,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import sys
 
-param  = sys.argv[1:] #recebe inicio e final
+param  = sys.argv[1:] #recebe inicio e final param[0]: inicio
+                                           # param[1]: destino
 
 def e_Vizinho(grafo,v1,v2):
     '''
@@ -44,29 +45,32 @@ def dijkstra2(grafo,inicio):
     '''
     visitados = []
     nao_visitados = []
-    atual = inicio
+    atual = inicio # primeio vertice a ser visitado é o inicio
     soma = 0
+    if inicio not in grafo:
+        return ""
 
     for i in grafo:
-            nao_visitados.append([i,sys.maxsize])
+            nao_visitados.append([i,sys.maxsize]) #criando uma lista de não visiados e colocando o peso de le em 'infinito'
 
-    nao_visitados[int(inicio)-1] = [inicio,0,inicio]
-    visitados.append(inicio)
+    nao_visitados[int(inicio)-1] = [inicio,0,inicio] #custo do inicial até ele é 0
+    visitados.append(inicio) #visitando o vertice inicial
 
-    while (len(visitados) != len(grafo)):
-        for i in range(len(grafo[atual])):
-            if grafo[atual][i][0] not in visitados:
-                if nao_visitados[int(grafo[atual][i][0]) -1][1] > grafo[atual][i][1] + soma:
-                    nao_visitados[int(grafo[atual][i][0]) -1] = [grafo[atual][i][0],grafo[atual][i][1] + soma,atual]
+    while (len(visitados) != len(grafo)): #enquanto a lista de visitados for menor que a quantidade de vvertices faça:
 
-        menor = [0,sys.maxsize]
-        for i in nao_visitados:
-            if i[0] not in visitados and (i[1] + soma) < (soma+menor[1]) and e_Vizinho(grafo,atual,i[0]):
+        for i in range(len(grafo[atual])): #percorre todos os vizinho do vertice i atualizando os custos
+            if grafo[atual][i][0] not in visitados: # se o vizinho não foi visitado
+                if nao_visitados[int(grafo[atual][i][0]) -1][1] > grafo[atual][i][1] + soma: # e se novo custo for menor do que está nesse vertice
+                    nao_visitados[int(grafo[atual][i][0]) -1] = [grafo[atual][i][0],grafo[atual][i][1] + soma,atual] # recebe novo custo
+
+        menor = [0,sys.maxsize] # inicializando menor
+        for i in nao_visitados: #percorre os vizinhosnão  visitados
+            if i[0] not in visitados and (i[1] + soma) < (soma+menor[1]) and e_Vizinho(grafo,atual,i[0]): # encontra o menor vizinho não visitado
                     menor = i
 
-        visitados.append(menor[0])
-        atual = menor[0]
-        soma = atualiza_soma(nao_visitados,atual,inicio)
+        visitados.append(menor[0]) #adiciona o vertice a lista de visitados
+        atual = menor[0] # atualiza o vertice atual para o que vai ser visitado agora
+        soma = atualiza_soma(nao_visitados,atual,inicio) # atualiza soma de acordo com o vertice atual
 
     return nao_visitados
 
@@ -89,11 +93,11 @@ def menor_caminho(lista,inicio,fim):
 
     return menor[::-1]
 # ---------------------main----------------------#
-dados = open("C:\\Users\\samue\\PycharmProjects\\graph\\obj.txt")
+dados = open("C:\\Users\\samue\\PycharmProjects\\graph\\obj.txt") #leitura dos dados do grafo
 
 grafo = {}
 i = 1
-for j in dados:
+for j in dados: #leitura do arquivo para filtras as informações e gerar o grafo
     j = j.replace('\n', '')
     a = []
     adjacencia = j.split(',')
@@ -106,31 +110,42 @@ for j in dados:
         grafo.update({vertice: a})
 dados.close()
 
-if len(param):
-    dk = dijkstra2(grafo, '5')
-    menor = (menor_caminho(dk, '5', '3'))
+if len(param): #se ao executar os parametros foram colocados na ordem certa
+    dk = dijkstra2(grafo, param[0])
+    menor = (menor_caminho(dk,param[0], param[1]))
 else:
-    menor = "0"
+    print("Erro: Não passou os parametros")
+    menor = 0
 
+if menor != 0:
+    #imprimindo o grafico graficamene kkkkk
+    G = nx.Graph()
 
-G = nx.Graph()
+    for i in grafo:
+        G.add_node(i)
+        for j in grafo[i]:
+            G.add_weighted_edges_from([(i, j[0], j[1])])
 
-for i in grafo:
-    G.add_node(i)
-    for j in grafo[i]:
-        G.add_weighted_edges_from([(i, j[0], j[1])])
+    pos = nx.planar_layout(G)
 
-pos = nx.layout.random_layout(G)
-node_sizes = []
+    print(pos)
+    node_sizes = []
+    cores = [""]*len(grafo)
+    pesos = []
+    for key in grafo:
+        node_sizes.append(500*len(grafo[key]))
+        if key in menor:
+            cores[int(key)-1]= "red"
+        else:
+            cores[int(key)-1] = "blue"
 
-for u in grafo:
-    node_sizes.append(500*len(grafo[u]))
-
-nodes = nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='blue',label=True)
-label = nx.draw_networkx_labels(G, pos,font_size=10,font_family= "Arial")
-edges = nx.draw_networkx_edges(G, pos, arrowstyle='->', arrowsize=10, width=3)
-
-ax = plt.gca()
-ax.set_axis_off()
-plt.savefig("Graph.png", format="PNG")
-plt.show()
+    nodes = nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color=cores,label=True)
+    label = nx.draw_networkx_labels(G, pos,font_size=10,font_family= "Arial")
+    edges = nx.draw_networkx_edges(G, pos, arrowstyle='->', arrowsize=5, width=3)
+    #edges_label = nx.draw_networkx_edge_labels(G, pos, edge_labels)
+    ax = plt.gca()
+    ax.set_axis_off()
+    titlo = "Menor caminho de " + str(menor[0]) + " a " +str(menor[len(menor)-1])
+    plt.title(titlo)
+    plt.savefig("Graph.png", format="PNG")
+    plt.show()
